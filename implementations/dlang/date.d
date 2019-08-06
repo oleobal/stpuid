f0103bc3bc9a3b53eb6a886d9760d60a11e1937d
Olivier Leobal <olivier.contrib@leobal.eu>
2019-08-07 00:12:47 +0200

    Fix date misbehavior

--- implementations/dlang/date.d
+++ implementations/dlang/date.d
@@ -17,11 +17,18 @@ enum DateFormat { iso, ms, slug };
 
 string getHelpUsage()
 {
-	return "date <ISO date | msecs timestamp | date slug>\n        [-o output] [-e epoch] [-h|--help] (for converting dates)";
+	return "date <ISO date | msecs timestamp | date slug | STPUID>\n        [-o output] [-e epoch] [-h|--help] (for converting dates)";
 }
 
 string getHelpText()
 {
+	string[string] options = [
+		"-o|--output": `The output format. One of `~to!string(acceptableDateFormats)~`; "iso" is the default, except if the input is iso, in which case the default is "ms"`,
+		"-e|--epoch": "Use the given epoch instead of 1970-01-01T00:00:00.000Z; can be either ISO or milliseconds since the default epoch",
+		"-eo|--epoch-out": "Use the given epoch for outputting the result; same format as --epoch; it defaults to 0, except if the output is ISO, in which case it defaults to the given epoch",
+		"-h|--help": "This help",
+	];
+	
 	return
 `Short & Trivial Practically Unique Identifier date tool
 Usage: stpuid `~getHelpUsage()~` 
@@ -33,12 +40,7 @@ Parameters:
    - a STPUID date slug
 
 Options:
- -o|--output : The output format. One of `~to!string(acceptableDateFormats)~`
-               "iso" is the default, except if the input is iso, in which case
-               the default is "ms"
- -e|--epoch  : Use the given epoch instead of 1970-01-01T00:00:00.000Z
-               can be either ISO or milliseconds since the default epoch
- -h|--help   : This help`;
+`~getFormattedOptions(options);
 }
 
 
@@ -50,6 +52,8 @@ int handle(string[] args)
 	string input = "";
 	long inputMs = 0;
 	long epoch=0; 
+	long epochOut=0; 
+	bool epochOutIsDefault=true;
 	DateFormat inType;
 	DateFormat outType = DateFormat.iso;
 
@@ -107,6 +111,27 @@ int handle(string[] args)
 			}
 			i++;
 		}
+		else if (arg.among!("-eo", "--epoch-out"))
+		{
+			try
+			{
+				epochOut = to!long(args[i+1]);
+			}
+			catch (ConvException e)
+			{
+				try
+				{
+					epochOut = getMilliseconds(args[i+1]);
+				}
+				catch (DateTimeException e)
+				{
+					stderr.writeln("Unintelligible input: "~args[i+1]);
+					return 1;
+				}
+			}
+			epochOutIsDefault=false;
+			i++;
+		}
 		
 
 		else // input
@@ -116,6 +141,17 @@ int handle(string[] args)
 			
 			if (input.length == 8 || input.length == 9)
 				inType = DateFormat.slug;
+				
+			if (input.length == 12)
+			{
+				input = arg[0..8];
+				inType = DateFormat.slug;
+			}
+			if (input.length == 14)
+			{
+				input = arg[0..9];
+				inType = DateFormat.slug;
+			}
 			
 			try
 			{
@@ -139,13 +175,15 @@ int handle(string[] args)
 	final switch (outType)
 	{
 		case DateFormat.iso:
-			writeln(getDate(epoch, inputMs).toISOExtString());
+			if (epochOutIsDefault)
+				epochOut = epoch;
+			writeln(getDate(epochOut, inputMs).toISOExtString());
 		break;
 		case DateFormat.ms:
-			writeln(inputMs-epoch);
+			writeln(inputMs-epochOut);
 		break;
 		case DateFormat.slug:
-			writeln(getDateSlug(inputMs-epoch));
+			writeln(getDateSlug(inputMs-epochOut));
 		break;
 	}
 	
--- implementations/dlang/utils.d
+++ implementations/dlang/utils.d
@@ -5,6 +5,7 @@ import std.conv;
 import core.time;
 import std.datetime.systime;
 import std.datetime.date:DateTimeException;
+import std.typecons:tuple, Tuple;
 
 string getApproximateHumanDuration(Duration d)
 {
@@ -82,4 +83,118 @@ string getApproximateHumanDuration(Duration d)
 	if (result.length == 0)
 		result = "just an instant";
 	return result;
+}
+
+
+
+/++
+ + formats option explanations, for help messages
+ +/
+string getFormattedOptions(string[string] options, uint lineLength=80)
+{
+	uint maxKeyLength = 0;
+	foreach (string k;options.keys)
+		if (k.length > maxKeyLength)
+			maxKeyLength = to!(uint)(k.length);
+	
+	maxKeyLength+=2+1+1; // 2 padding left, 1 colon, 1 padding right
+	int textLength = lineLength - maxKeyLength;
+	string output="";
+	
+	if (textLength < 40) // separate lines
+	{
+		textLength = lineLength-2;
+		string padding = "  ";
+		// TODO
+	}
+	else
+	{
+		string padding = "";
+		for (int i=0; i<maxKeyLength;i++) // there's probably a better way..
+			padding~=" "; //.. but it's not under "12.9 Array operations"
+		foreach(string k, v; options)
+		{
+			auto lines = getLineBrokenText(v, textLength);
+			if (lines.length > 0)
+			{
+				string firstPadding = "";
+				for (int i=0; i<maxKeyLength-2-1-1-k.length;i++) //ok, getting stpuid
+					firstPadding~=" ";
+				
+				output~="  "~k~firstPadding~": "~lines[0]~"\n";
+				foreach (string t; lines[1..$])
+				{
+					output~=padding~t~"\n";
+				}
+			}
+			else
+				output~="  "~k~": ?\n";
+		}
+	}
+	return output;
+}
+
+
+string[] getLineBrokenText(string text, uint lineLength)
+{
+	alias Word = Tuple!(string, "text", bool, "isWhitespace");
+	
+	string addUpLine(ref Word[] line, uint lineLength, bool manualLineFeed)
+	{
+		// add up line, break if necessary
+		auto total = 0;
+		auto nbWords = 0;
+		foreach(Word word; line)
+		{
+			if (total + word.text.length < lineLength)
+			{
+				total+= word.text.length;
+				nbWords+=1;
+			}
+			// TODO special case words with length>lineLength
+		}
+		string finishedLine = "";
+		for (int j=0; j<nbWords; j++)
+		{
+			if (finishedLine.length > 0 || manualLineFeed  || !line[j].isWhitespace)
+				finishedLine~=line[j].text;
+			// TODO handle whitespace better
+		}
+		line = line[nbWords..$];
+		return finishedLine;
+	}
+	
+	
+	string[] output = [];
+	Word[] lineBuf = [];
+	
+	for (uint i=0; i< text.length; i++)
+	{
+		if (text[i] == ' ' || text[i] == '\t')
+		{
+			if (lineBuf.length == 0 || !lineBuf[$-1].isWhitespace)
+			{
+				lineBuf~=Word("", true);
+			}
+			lineBuf[$-1].text~=text[i]; 
+		}
+		else if (text[i] != '\n')
+		{
+			if (lineBuf.length == 0 || lineBuf[$-1].isWhitespace)
+			{
+				lineBuf~=Word("", false);
+			}
+			lineBuf[$-1].text~=text[i]; 
+		}
+		else
+		{
+			output ~= addUpLine(lineBuf, lineLength, true);
+		}
+	}
+	
+	while(lineBuf.length > 0)
+		output ~= addUpLine(lineBuf, lineLength, false);
+	
+	
+	return output;
 }
